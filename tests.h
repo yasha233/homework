5bb75cca9eddb0251f7ee012564c4b6ac4d664a8
yasha233 <126241940+yasha233@users.noreply.github.com>
2024-03-12 15:46:40 +0300

    Update tests.h

--- tests.h
+++ tests.h
@@ -1,17 +1,17 @@
 #include"function.h"
 namespace numb1{
 void test() {
-	assert(numb1::sqr(3) == 9);
-	assert(numb1::isEven(4) == true);
+	assert(sqr(3) == 9);
+	assert(isEven(4) == true);
 	std::cout << "test ok" << std::endl;
 }
 }
 namespace funct2{
 void testIsEven() {
-	assert(funct2::isEven(4) == true);
-	assert(funct2::isEven(5) == false);
-	assert(funct2::isEven(0) == true);
-	assert(funct2::isEven(13) == false);
+	assert(isEven(4) == true);
+	assert(isEven(5) == false);
+	assert(isEven(0) == true);
+	assert(isEven(13) == false);
 	std::cout << "isEven ok" << std::endl;
 }
 }
