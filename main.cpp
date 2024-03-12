842836fef5a6467393e54852d6d0d06554bf8305
yasha233 <126241940+yasha233@users.noreply.github.com>
2024-03-12 17:28:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,8 @@
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	std::vector<int> h = { 3,5,4,8 };
-	auto results = numb1::filter(h, numb1::isEven);
-	std::cout <<" Results: ";
-	for (auto res : results)
-	{
-		std::cout << res << " ";
-	}
-	std::cout << std::endl;
+	numb1::test2();
 	numb1::test();
-
-	std::vector<int> n = {1,2,4};
-	auto results2 = numb1::map(n,numb1::sqr);
-	std::cout << "Results 2: ";
-	for (auto res : results2)
-	{
-		std::cout << res << " ";
-	}
-	std::cout << std::endl;
 //funct2::Game();
 funct2::testIsEven();
 return 0;
